f2e20406581b251121a95830b18d5fbac4edc443
Alvarino <83028364+Rafael-A8@users.noreply.github.com>
2026-04-07 11:28:31 -0300

    5 - Update AppLogo and AppLogoIcon components for branding consistency; modify AppSidebar for navigation updates

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { Link } from '@inertiajs/vue3';
-import { BookOpen, FolderGit2, LayoutGrid, MessageCircle } from 'lucide-vue-next';
+import { Github, LayoutGrid, MessageCircle } from 'lucide-vue-next';
 import AppLogo from '@/components/AppLogo.vue';
 import NavFooter from '@/components/NavFooter.vue';
 import NavMain from '@/components/NavMain.vue';
@@ -33,14 +33,9 @@ const mainNavItems: NavItem[] = [
 
 const footerNavItems: NavItem[] = [
     {
-        title: 'Repository',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: FolderGit2,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
+        title: 'Rafael Alvarino © 2026',
+        href: 'https://github.com/Rafael-A8',
+        icon: Github,
     },
 ];
 </script>
